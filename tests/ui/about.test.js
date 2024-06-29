8316838f8d2e2035918b1e6fa5695930c3181e9f
Kris <kkristiqn1@gmail.com>
2024-06-29 18:15:01 +0100

    Fixed ui tests

--- tests/ui/about.test.js
+++ tests/ui/about.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test.only('Check about page', async ({ page }) => {
+test('Check about page', async ({ page }) => {
   await page.goto('http://localhost:8080/about');
   const heading = await page.$('h1');
   const text = await heading.textContent();
@@ -8,9 +8,9 @@ test.only('Check about page', async ({ page }) => {
 });
 
 test('Check about page test', async ({ page }) => {
-  await page.goto('http://localhost:8090/about');
+  await page.goto('http://localhost:8080/about');
   const heading = await page.$('a');
   const text = await heading.textContent();
-  expect(text).toBe('This is my gardes list');
+  expect(text).toBe('Home');
 });
 
